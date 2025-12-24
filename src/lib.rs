7da3662807ca6e90300a65759fb53fc571bcbed3
Fenhl <fenhl@fenhl.net>
2025-12-24 09:41:51 +0000

    Make sleep module public

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod cli;
 pub mod domain;
 pub mod errors;
 pub mod report;
-#[cfg_attr(not(target_os = "linux"), path = "sleep-fallback.rs")] mod sleep;
+#[cfg_attr(not(target_os = "linux"), path = "sleep-fallback.rs")] pub mod sleep;
 pub mod subcommands;
 pub mod traits;
 pub mod utils;
